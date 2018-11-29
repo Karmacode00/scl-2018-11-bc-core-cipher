71ee43592e319e0fd4af729a7095041e489a8b98
AndreaL <alafertteo@gmail.com>
2018-11-29 19:07:06 -0300

    html y css

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,27 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (str, num) => {
+    // Comienza función encode
+    num = parseInt(num);
+    let result = '';
+    // Se moverá de a una posición a lo largo de lapalabra entregada
+    for (let i = 0; i < str.length; i++) {
+      let char = str[i];
+      // let charCode = char.charCodeAt(i);
+      if (char.match(/[A-Z]/i)) { // Si lo que ingresamos es letra 
+        let charCode = str.charCodeAt(i);
+        // AsCii
+        // Mayúsculas 
+        if ((charCode >= 65) && (charCode <= 90)) {
+          char = String.fromCharCode(((charCode - 65 + parseInt(num)) % 26) + 65);
+        }
+        // Minusculas 
+        else if ((charCode >= 97) && (charCode <= 122)) {
+          char = String.fromCharCode(((charCode - 97 + parseInt(num)) % 26) + 97);
+        }
+      }
+      result += char;
+    }
+    return result;
   },
   decode: () => {
     /* Acá va tu código */
