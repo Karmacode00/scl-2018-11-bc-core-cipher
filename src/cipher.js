0ca09ab330f93e6dc6a33aa51be2643e16c71cbf
AndreaL <alafertteo@gmail.com>
2018-11-30 10:40:53 -0300

    cambios en función de cifrar

--- src/cipher.js
+++ src/cipher.js
@@ -7,7 +7,7 @@ window.cipher = {
     let newNumber = '';
     for (let i = 0; i < textEncode.length; i++) {
       letterCipher = textEncode.charCodeAt(i);
-      if (letterCipher === 32) { 
+      if (letterCipher === 32) {
         newLetter = String.fromCharCode(letterCipher);
         resultEncode += newLetter
       } else if (letterCipher >= 65 && letterCipher <= 90) {
@@ -17,8 +17,8 @@ window.cipher = {
         newNumber = ((letterCipher - 65 + offsetNum) % 26 + 65);
         newLetter = String.fromCharCode(newNumber);
         resultEncode += newLetter
-      }else if(letterCipher >= 97 && letterCipher <= 122){
-        while (offsetNum < 0){
+      } else if (letterCipher >= 97 && letterCipher <= 122) {
+        while (offsetNum < 0) {
           offsetNum = offsetNum + 26
         }
         newNumber = ((letterCipher - 97 + offsetNum) % 26 + 97);
