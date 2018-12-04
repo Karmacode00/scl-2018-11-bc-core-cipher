407f1bdc146ae5f868c81d61e0fb8d7e36ce94ef
AndreaL <alafertteo@gmail.com>
2018-12-04 09:39:01 -0300

    docs

---
+++ docs/cipher.js
@@ -0,0 +1,70 @@
+window.cipher = {
+  encode: (textEncode, offsetNum) => {
+    let resultEncode = '';
+    let letterCipher = '';
+    let newLetter = '';
+    let newNumber = '';
+    for (let i = 0; i < textEncode.length; i++){
+      letterCipher = textEncode.charCodeAt(i);
+    if(letterCipher >= 65 && letterCipher <= 90){
+        while (offsetNum < 0) {
+          offsetNum = offsetNum + 26
+        }
+        newNumber = ((letterCipher - 65 + offsetNum) % 26 + 65);
+        newLetter = String.fromCharCode(newNumber);
+        resultEncode += newLetter
+      }else if (letterCipher >= 97 && letterCipher <= 122) {
+        while (offsetNum < 0) {
+          offsetNum = offsetNum + 26
+        }
+        newNumber = ((letterCipher - 97 + offsetNum) % 26 + 97);
+        newLetter = String.fromCharCode(newNumber);
+        resultEncode += newLetter
+      }else{
+        newLetter = String.fromCharCode(letterCipher);
+        resultEncode += newLetter
+      }
+    }
+    return resultEncode;
+  },
+  decode: (textDecode, offsetNumDec) => {
+    let resultDecode = '';
+    let letterDecode = '';
+    let newLetter = '';
+    let newNumber = '';
+    let decodeUpper = '';
+    let decodeLower = '';
+    for(let i=0; i<textDecode.length; i++){
+      letterDecode = textDecode.charCodeAt(i);
+      if(letterDecode >= 65 && letterDecode <= 90){
+        while (offsetNumDec < 0){
+          offsetNumDec = offsetNumDec + 26
+        }
+        decodeUpper = (letterDecode - 65 - offsetNumDec)
+        while (decodeUpper < 0){
+          decodeUpper = decodeUpper + 26
+        }
+        newNumber = decodeUpper % 26 + 65
+        newLetter = String.fromCharCode(newNumber);
+        resultDecode += newLetter       
+      }else if(letterDecode >= 97 && letterDecode <= 122){
+        while (offsetNumDec < 0){
+          offsetNumDec = offsetNumDec + 26
+        }
+        decodeLower = (letterDecode - 97 - offsetNumDec)
+        while (decodeLower < 0){
+          decodeLower = decodeLower + 26
+        }
+        newNumber = decodeLower % 26 + 97;
+        newLetter = String.fromCharCode(newNumber);
+        resultDecode += newLetter
+      }else{
+        newLetter = String.fromCharCode(letterDecode);
+        resultDecode += newLetter
+      }
+    }
+    return resultDecode;
+  }
+};
+
+
---
+++ docs/index.js
@@ -0,0 +1,59 @@
+window.onload = () => {
+  document.getElementById('definition').style.display = 'none';
+  document.getElementById('instructions').style.display = 'none';
+
+  // Toggle
+  document.getElementById('btnDefinition').addEventListener('click', () => {
+    const definicion = document.getElementById('definition');
+    if (definicion.style.display === 'none') {
+      definicion.style.display = 'block';
+    } else {
+      definicion.style.display = 'none';
+    }
+  });
+
+  // Toggle
+  document.getElementById('btnInstructions').addEventListener('click', () => {
+    const instrucciones = document.getElementById('instructions');
+    if (instrucciones.style.display === 'none') {
+      instrucciones.style.display = 'block';
+    } else {
+      instrucciones.style.display = 'none';
+    }
+  });
+
+  // Evento click limpiar
+  document.getElementById('limpiar').addEventListener('click', () => {
+    document.getElementById('text').value = '';
+    document.getElementById('output').value = '';
+    document.getElementById('offset').value = '';
+  });
+
+  // Evento click copiar
+  document.getElementById('copiar').addEventListener('click', () => {
+    /* Get the text field */
+    let copyText = document.getElementById('output');
+    /* Select the text field */
+    copyText.select();
+    /* Copy the text inside the text field */
+    document.execCommand('copy');
+    /* Alert the copied text */
+    alert('Copiaste este mensaje: ' + copyText.value);
+  });
+
+  // Evento click texto a encode
+  document.getElementById('btnEncode').addEventListener('click', () => {
+    let textEncode = document.getElementById('text').value;
+    let offsetNum = document.getElementById('offset').value;
+    const resultEncode = window.cipher.encode(textEncode, offsetNum);
+    document.getElementById('output').value = resultEncode;
+  });
+
+  // Evento click texto a decode
+  document.getElementById('btnDecode').addEventListener('click', () => {
+    let textDecode = document.getElementById('text').value;
+    let offsetNumDec = document.getElementById('offset').value;
+    const resultDecode = window.cipher.decode(textDecode, offsetNumDec);
+    document.getElementById('output').value = resultDecode;
+  });
+}
